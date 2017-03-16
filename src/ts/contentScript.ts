fa1bd4f8cfd6970604d3677fa778fd846892e319
broven <zycode277@163.com>
2017-03-16 17:49:39 +0800

    #9

--- src/ts/contentScript.ts
+++ src/ts/contentScript.ts
@@ -3,6 +3,8 @@ import * as api from "./data"
 import {debug} from "./log"
 let title, url, path, origin, startTime = new Date(), endTime, duration = 0
 let sh = new Shadow()
+
+startTime = new Date()
 window.addEventListener('load', function () {
   console.log('document ready');
 
@@ -12,7 +14,6 @@ window.addEventListener('load', function () {
   api.getRecords('2017-03-13', new Date().getTime()).then(recordes => {
     debug(recordes)
   })
-  getPageInfo()
 })
 /**
  * 当页面可视性发生改变时会触发的事件
@@ -28,6 +29,7 @@ document.addEventListener("visibilitychange", function () {
 window.addEventListener("beforeunload",stop)
 
 function stop() {
+  getPageInfo()
  duration = sh.stop()
  endTime = new Date()
  api.saveRecord(title, origin, path, startTime, endTime, duration)
@@ -37,7 +39,6 @@ function getPageInfo() {
   url = window.location
   origin = url.hostname
   path = url.pathname
-  startTime = new Date()
 }
 // debug
 // window.addEventListener("mousemove", stop)
