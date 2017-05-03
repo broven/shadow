d13d6f07636967d8071155246d013f7aad8542d0
meta <zycode277@163.com>
2017-05-03 22:23:48 +0800

    util->utils

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,10 +1,5 @@
-export function formatDate(date) {
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
-}
 
  /**
- *
- *
  * @param {object} obj 需要排序的obj
  * @param {string} sortKey 根据这个key来进行排序
  * @return {array} 返回经过排序的数组,降序排列
