d2eabb1b1a261832796f20b5a10ffa7f092cdf7b
meta <zycode277@163.com>
2017-05-09 21:18:15 +0800

    修改kama配置文件

--- karma.conf.js
+++ karma.conf.js
@@ -52,8 +52,6 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
 
 
     // start these browsers
